287e6b81a9aead81adabadb357f6abd82d941a9b
danary gonzalez <danarygo@gmail.com>
2022-09-15 04:03:56 +0000

    completed javascript arrays

---
+++ javascript-arrays/main.js
@@ -0,0 +1,18 @@
+// Colors Array
+var colors = ['red', 'white', 'blue'];
+console.log('value of colors[0]:', colors[0]);
+console.log('value of colors[1]:', colors[1]);
+console.log('value of colors[2]:', colors[2]);
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+colors[2] = 'green';
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+console.log('value of colors:', colors);
+
+// Students Array
+var students = ['Danary', 'Matt', 'Tom', 'Samantha'];
+var numberOfStudents = students.length;
+console.log('There are ' + numberOfStudents + ' students in the class.');
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+console.log('value of lastStudent:', lastStudent);
+console.log('value of students', students);
